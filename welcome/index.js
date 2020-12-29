995307db7d90edef46dbf4d0260ffedd964de961
Shodan <mycrash_forever@hotmail.com>
2020-12-29 00:24:52 -0500

    redirec

---
+++ welcome/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   
+    // typing text animation script
+   
+    var typed = new Typed(".typing", {
+        strings: ["Redirigiendo..."],
+        typeSpeed: 50,
+        backSpeed: 20,
+        loop: true
+    });
+});
